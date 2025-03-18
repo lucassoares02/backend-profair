220bd0a83fcf85f222a71ed112f9e5f15e4fb05e
lucassoares02 <lucas.soares02@hotmail.com>
2025-03-17 21:05:21 -0300

    add cors headers

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ require("newrelic");
 
 const routes = require("@routes");
 
-app.use(cors({ origin: "*", allowedHeaders: "X-Requested-With, Content-Type, codacesso, action, platform", }));
+app.use(cors({ origin: "*", allowedHeaders: "X-Requested-With, Content-Type, agentRequest, platform", }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", routes); ''
