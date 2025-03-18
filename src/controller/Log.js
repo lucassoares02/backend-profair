d577a4d51fd4319ab9c3226870e9fe7536eb306a
lucassoares02 <lucas.soares02@hotmail.com>
2025-03-17 21:21:15 -0300

    add cors headers

--- src/controller/Log.js
+++ src/controller/Log.js
@@ -6,9 +6,6 @@ const Log = {
   async InsertLog(req, res, next) {
     logger.info("Insert Logs");
 
-    console.log(req)
-
-
     const logData = {
       ip: req.ip || req.headers["x-forwarded-for"] || req.connection.remoteAddress,
       userAgent: req.headers["user-id"],
@@ -19,11 +16,7 @@ const Log = {
       timestamp: new Date().toISOString().slice(0, 19).replace("T", " "),
     };
 
-    console.log(logData);
-
     const query = `INSERT INTO log (ip, userAgent, method, route, body, queryParams, timestamp) VALUES ('${logData.ip}', '${logData.userAgent}', '${logData.method}', '${logData.route}', '${JSON.stringify(logData.body)}', '${JSON.stringify(logData.queryParams)}', '${logData.timestamp}'); SHOW WARNINGS;`;
-
-    console.log(query);
 
     connection.query(query, (error, results, fields) => {
       next();
