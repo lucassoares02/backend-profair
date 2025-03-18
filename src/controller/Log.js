c82d50a14462acfd79e66f550fe55286f43f1d45
lucassoares02 <lucas.soares02@hotmail.com>
2025-03-17 21:02:10 -0300

    add agent request

--- src/controller/Log.js
+++ src/controller/Log.js
@@ -6,9 +6,10 @@ const Log = {
   async InsertLog(req, res, next) {
     logger.info("Insert Logs");
 
+
     const logData = {
       ip: req.ip || req.headers["x-forwarded-for"] || req.connection.remoteAddress,
-      userAgent: req.headers["user-agent"],
+      userAgent: req.headers.agentRequest,
       method: req.method,
       route: req.originalUrl,
       body: req.method == 'GET' ? req.body : req.method == 'POST' ? req.body : req.method == 'PUT' ? req.body : req.method == 'DELETE' ? req.body : req.body,
