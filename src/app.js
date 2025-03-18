847cfff0a18ed1b903c1e2c1562e6c7239923ac7
lucassoares02 <lucas.soares02@hotmail.com>
2025-03-18 19:44:06 -0300

    add headers user

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ require("newrelic");
 
 const routes = require("@routes");
 
-app.use(cors({ origin: "*", allowedHeaders: "X-Requested-With, Content-Type, user-id, platform", }));
+app.use(cors({ origin: "*", allowedHeaders: "X-Requested-With, Content-Type, user-id, platform", exposedHeaders: ["user-id", "platform"] }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", routes); ''
