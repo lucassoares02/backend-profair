cf69535039fe7773ae427133370daa905899d9d9
lucassoares02 <lucas.soares02@hotmail.com>
2025-03-19 21:37:34 -0300

    remote unecessary headers

--- src/controller/Log.js
+++ src/controller/Log.js
@@ -6,25 +6,17 @@ const Log = {
   async InsertLog(req, res, next) {
     logger.info("Insert Logs");
 
-    console.log("=============================================");
-    console.log("==================USER ID====================");
-    console.log("=============================================");
-    console.log(req.headers["user-id"]);
-    console.log("=============================================");
-
     const logData = {
-      ip: req.ip || req.headers["x-forwarded-for"] || req.connection.remoteAddress,
       userAgent: req.headers["user-id"],
       method: req.method,
       route: req.originalUrl,
       body: req.method == 'GET' ? req.body : req.method == 'POST' ? req.body : req.method == 'PUT' ? req.body : req.method == 'DELETE' ? req.body : req.body,
-      queryParams: req.query,
       timestamp: new Date().toISOString().slice(0, 19).replace("T", " "),
     };
 
     console.log(logData);
 
-    const query = `INSERT INTO log (ip, userAgent, header, method, route, body, queryParams, timestamp) VALUES ('${logData.ip}', '${logData.userAgent}', '${logData.header}', '${logData.method}', '${logData.route}', '${JSON.stringify(logData.body)}', '${JSON.stringify(logData.queryParams)}', '${logData.timestamp}'); SHOW WARNINGS;`;
+    const query = `INSERT INTO log ( userAgent, header, method, route, body, timestamp) VALUES ('${logData.userAgent}', '${logData.header}', '${logData.method}', '${logData.route}', '${JSON.stringify(logData.body)}',  '${logData.timestamp}'); SHOW WARNINGS;`;
 
     connection.query(query, (error, results, fields) => {
       next();
