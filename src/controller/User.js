0c06f9a5550c5a11ed3786080d6daebb660d55c7
lucassoares02 <lucas.soares02@hotmail.com>
2025-04-01 12:37:50 -0300

    add promise request

--- src/controller/User.js
+++ src/controller/User.js
@@ -19,7 +19,7 @@ const User = {
 
     const queryConsult = "select codAcesso, codOrganization, direcAcesso from acesso where codAcesso = " + codacesso;
 
-    connection.query(queryConsult, async (error, resultsTop, fields) => {
+    connection.promise.query(queryConsult, async (error, resultsTop, fields) => {
       if (error) {
         return res.status(400).send(error);
       } else {
@@ -81,7 +81,7 @@ const User = {
 
             // `SET sql_mode = ''; SELECT acesso.codAcesso, acesso.direcAcesso, associado.razaoAssociado AS nomeForn, associado.cnpjAssociado AS cnpjForn, acesso.codUsuario, associado.codAssociado AS codForn, consultor.nomeConsult, consultor.cpfConsult, FORMAT(IFNULL(sum(mercadoria.precoMercadoria*pedido.quantMercPedido), 0), 2, 'de_DE') as valorPedido FROM acesso join consultor on acesso.codUsuario = consultor.codConsult join relaciona on relaciona.codAssocRelaciona = consultor.codConsult join associado on associado.codAssociado = relaciona.codConsultRelaciona left join pedido on pedido.codAssocPedido = associado.codAssociado left join mercadoria on mercadoria.codMercadoria = pedido.codMercPedido WHERE acesso.codAcesso = '${codacesso}'`;
 
-            connection.query(queryClient, (error, results) => {
+            connection.promise.query(queryClient, (error, results) => {
               if (error) {
                 return res.status(400).send(error);
               } else {
