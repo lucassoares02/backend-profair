c2fbca112b431c4860cf0d23a397cfa62df2cf75
lucassoares02 <lucas.soares02@hotmail.com>
2025-04-03 15:22:11 -0300

    change status negociacao_loja

--- src/controller/Multishow.js
+++ src/controller/Multishow.js
@@ -47,7 +47,7 @@ const Notice = {
   async getQueryPosEvent(req, res) {
     logger.info("Get Query Pre Event");
 
-    const queryConsult = "SET sql_mode = ''; select CONCAT('UPDATE multishow_b2b.negociacoes_lojas SET id_loja = ', nl.id_loja, ' WHERE id_negociacao = ', nl.id_negociacao, ' and id_negociacao_loja = ', nl.id_negociacao_loja, ';') as 'query' from log l join acesso a on a.codAcesso = l.userAgent join consultor c on c.codConsult = a.codUsuario join relaciona r on r.codAssocRelaciona = c.codConsult join negociacao_loja nl on nl.id_loja = r.codConsultRelaciona where l.route like '%getusermore%' and a.direcAcesso = 2 group by nl.id_loja, nl.id_negociacao";
+    const queryConsult = "SET sql_mode = ''; select CONCAT('UPDATE multishow_b2b.negociacoes_lojas SET id_loja = ', nl.id_loja, ' WHERE id_negociacao = ', nl.id_negociacao, ' and id_negociacao_loja = ', nl.id_negociacao_loja, ';') as 'query' from log l join acesso a on a.codAcesso = l.userAgent join consultor c on c.codConsult = a.codUsuario join relaciona r on r.codAssocRelaciona = c.codConsult join negociacao_loja nl on nl.id_loja = r.codConsultRelaciona where l.route like '%getusermore%' and a.direcAcesso = 2 and nl.status = 0 group by nl.id_loja, nl.id_negociacao";
     
     
     connection.query(queryConsult, (error, results, fields) => {
@@ -269,6 +269,7 @@ const Notice = {
         id_negociacao_loja: element["id_negociacao_loja"],
         id_negociacao: element["id_negociacao"],
         id_loja: element["id_loja"],
+        status: 0
       });
     }
 
