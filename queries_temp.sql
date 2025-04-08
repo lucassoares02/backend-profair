aa01422dc09ec4aa5d8ac736421c6cbc5ac2da19
lucassoares02 <lucas.soares02@hotmail.com>
2025-04-08 17:57:20 -0300

    fix presents event clients

--- queries_temp.sql
+++ queries_temp.sql
@@ -1,21 +1,21 @@
-SET sql_mode = '';
+SET
+    sql_mode = '';
 
-SELECT  
-    a.codAssociado,
-    a.razaoAssociado AS razao,
-    a.cnpjAssociado,
-    SUM(IF(p.codFornPedido = ${codprovider}, p.quantMercPedido * m.precoMercadoria, 0)) AS valorTotal,
-    SUM(IF(p.codFornPedido = ${codprovider}, p.quantMercPedido, 0)) AS volumeTotal
-FROM (
-    -- Lista de associados cujos consultores estavam no log
-    SELECT DISTINCT a.codAssociado, a.razaoAssociado, a.cnpjAssociado
-    FROM log l
-    JOIN acesso ac ON ac.codAcesso = l.userAgent
-    JOIN consultor c ON c.codConsult = ac.codUsuario
-    JOIN relaciona r ON r.codAssocRelaciona = c.codConsult
-    JOIN associado a ON a.codAssociado = r.codConsultRelaciona
-) AS a
-LEFT JOIN pedido p ON p.codAssocPedido = a.codAssociado
-LEFT JOIN mercadoria m ON m.codMercadoria = p.codMercPedido
-GROUP BY a.codAssociado
-ORDER BY valorTotal DESC;
+SELECT
+    (COUNT(DISTINCT pedido.codAssocPedido) * 100.0) / (
+         SELECT COUNT(DISTINCT r.codConsultRelaciona) AS total
+       FROM log l
+       JOIN acesso a ON a.codAcesso = l.userAgent
+       JOIN relaciona r ON r.codAssocRelaciona = a.codUsuario
+    ) AS porcentagem,
+    COUNT(DISTINCT pedido.codAssocPedido) AS realizados,
+    (
+       SELECT COUNT(DISTINCT r.codConsultRelaciona) AS total
+       FROM log l
+       JOIN acesso a ON a.codAcesso = l.userAgent
+       JOIN relaciona r ON r.codAssocRelaciona = a.codUsuario
+    ) AS total
+FROM
+    pedido
+WHERE
+    pedido.codFornPedido = 10766
--- src/controller/Graphs.js
+++ src/controller/Graphs.js
@@ -13,7 +13,30 @@ const Graphs = {
 
     const { codprovider } = req.params;
 
-    const queryConsult = `SET sql_mode = ''; SELECT (COUNT(DISTINCT pedido.codAssocPedido) * 100.0) / (SELECT COUNT(*) FROM associado) AS porcentagem, COUNT(DISTINCT pedido.codAssocPedido) AS realizados, (SELECT COUNT(*) FROM associado) AS total FROM pedido WHERE pedido.codFornPedido = ${codprovider}`;
+    // const queryConsult = `SET sql_mode = ''; SELECT (COUNT(DISTINCT pedido.codAssocPedido) * 100.0) / (SELECT COUNT(*) FROM associado) AS porcentagem, COUNT(DISTINCT pedido.codAssocPedido) AS realizados, (SELECT COUNT(*) FROM associado) AS total FROM pedido WHERE pedido.codFornPedido = ${codprovider}`;
+    const queryConsult = `
+    SET
+    sql_mode = '';
+
+SELECT
+    (COUNT(DISTINCT pedido.codAssocPedido) * 100.0) / (
+         SELECT COUNT(DISTINCT r.codConsultRelaciona) AS total
+       FROM log l
+       JOIN acesso a ON a.codAcesso = l.userAgent
+       JOIN relaciona r ON r.codAssocRelaciona = a.codUsuario
+    ) AS porcentagem,
+    COUNT(DISTINCT pedido.codAssocPedido) AS realizados,
+    (
+       SELECT COUNT(DISTINCT r.codConsultRelaciona) AS total
+       FROM log l
+       JOIN acesso a ON a.codAcesso = l.userAgent
+       JOIN relaciona r ON r.codAssocRelaciona = a.codUsuario
+    ) AS total
+FROM
+    pedido
+WHERE
+    pedido.codFornPedido = ${codprovider}
+    `;
 
     connection.query(queryConsult, (error, results, fields) => {
       if (error) {
