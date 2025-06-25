a964b8e13e1705fa1cd84a638406f90347039e05
lucassoares02 <lucas.soares02@hotmail.com>
2025-06-24 21:04:49 -0300

    add dynamic token send notification

--- src/controller/Notification.js
+++ src/controller/Notification.js
@@ -4,32 +4,9 @@ const admin = require('firebase-admin');
 
 const Notification = {
 
-  async insertNotification(req, res, next) {
-    logger.info("Insert Notifications");
-    const data = req.body;
-
-    let params = {
-      table: "notifications",
-      data: [data],
-    };
-    return Insert(params)
-      .then(async (resp) => {
-        sendNotification(data.title, data.content, data.token);
-        res.status(200).send({
-          message: "Notification Inserted Successfully",
-          data: resp,
-        });
-      })
-      .catch((error) => {
-        res.status(400).send(error);
-      });
-  },
-
-  async sendNotification(req, res, next) {
+   async sendNotification(req, res, next) {
     logger.info("Send Notifications");
-    const { title, body } = req.body;
-
-    const token = "ci3KaERkT-a_nVvGovOpG7:APA91bH_ImimW8LTRB-msmJdfDCtoqZozsoXjNLGk7LaJgbMx59LOUuHSmEIAr9RGdIt5_254BWgZsztol77tlPruSu9THB2ouidglNsacRM48aiNXq9zlQ";
+    const { title, body, token } = req.body;
 
     const decodedKey = Buffer.from(process.env.FIREBASE_KEY_BASE64, 'base64').toString('utf8');
     const serviceAccount = JSON.parse(decodedKey);
@@ -59,6 +36,29 @@ const Notification = {
       res.status(500).send({ message: "Error sending notification", error });
     }
   },
+
+  async insertNotification(req, res, next) {
+    logger.info("Insert Notifications");
+    const data = req.body;
+
+    let params = {
+      table: "notifications",
+      data: [data],
+    };
+    return Insert(params)
+      .then(async (resp) => {
+        this.sendNotification(data.title, data.content, data.token);
+        res.status(200).send({
+          message: "Notification Inserted Successfully",
+          data: resp,
+        });
+      })
+      .catch((error) => {
+        res.status(400).send(error);
+      });
+  },
+
+ 
 
 };
 
