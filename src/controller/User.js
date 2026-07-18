aedf5bfb9cbbc881fe533ed628d478808d414f93
lucassoares02 <lucas.soares02@hotmail.com>
2026-07-18 07:50:20 -0300

    add is present in get double

--- src/controller/User.js
+++ src/controller/User.js
@@ -128,7 +128,12 @@ const User = {
 
             // `SET sql_mode = ''; SELECT acesso.codAcesso, acesso.direcAcesso, associado.razaoAssociado AS nomeForn, associado.cnpjAssociado AS cnpjForn, acesso.codUsuario, associado.codAssociado AS codForn, consultor.nomeConsult, consultor.cpfConsult, FORMAT(IFNULL(sum(mercadoria.precoMercadoria*pedido.quantMercPedido), 0), 2, 'de_DE') as valorPedido FROM acesso join consultor on acesso.codUsuario = consultor.codConsult join relaciona on relaciona.codAssocRelaciona = consultor.codConsult join associado on associado.codAssociado = relaciona.codConsultRelaciona left join pedido on pedido.codAssocPedido = associado.codAssociado left join mercadoria on mercadoria.codMercadoria = pedido.codMercPedido WHERE acesso.codAcesso = '${codacesso}'`;
 
-            queryUpdate = `update negociacao_loja set active = 1 where `;
+            const queryPresent = `UPDATE acesso SET is_present = 1 WHERE codAcesso = ${codacesso}`;
+            connection.query(queryPresent, (error) => {
+              if (error) {
+                console.log(`Error update is_present acesso: ${error}`);
+              }
+            });
 
             connection.query(queryClient, (error, results) => {
               if (error) {
